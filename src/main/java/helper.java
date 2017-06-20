382ccd22ae7ee48fd8e4d5f711edf21b5bb0abd7
Forest Newark <forest.newark@gmail.com>
2017-06-20 14:48:44 -0400

    getting close on the function

--- src/main/java/helper.java
+++ src/main/java/helper.java
@@ -1,4 +1,7 @@
+import org.apache.commons.math3.analysis.interpolation.HermiteInterpolator;
 import org.apache.commons.math3.analysis.interpolation.LinearInterpolator;
+import org.apache.commons.math3.analysis.interpolation.SmoothingPolynomialBicubicSplineInterpolator;
+import org.apache.commons.math3.analysis.polynomials.PolynomialFunction;
 import org.apache.commons.math3.analysis.polynomials.PolynomialSplineFunction;
 
 /**
@@ -8,9 +11,21 @@ public class helper {
 
     public static double linearInterp(double[] x, double[] y, double xi) {
         // return linear interpolation of (x,y) on xi
+
+
         LinearInterpolator li = new LinearInterpolator();
         PolynomialSplineFunction psf = li.interpolate(x, y);
         double yi = psf.value(xi);
         return yi;
     }
+
+    public static double Hermite(double[] x, double[] y, double xi){
+
+        HermiteInterpolator interpolator = new HermiteInterpolator();
+
+        return 0.0;
+    }
+
+
+
 }
